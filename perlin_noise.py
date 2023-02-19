3ee3c0ff27242c481eba2bd07a836aeeb18e85f0
psichose <chatryjulien@gmail.com>
2023-02-20 00:47:46 +0100

    Bug fixes + switched to np arrays

--- perlin_noise.py
+++ perlin_noise.py
@@ -6,22 +6,24 @@ from matplotlib import cm
 import numpy as np
 
 class Perlin_Noise():
-	def __init__(self, sizes = (21,21), mesh_size = (4,4)):
+	def __init__(self, sizes, mesh_size):
 		self.sizes = sizes #Width / Height
 		self.dim = len(sizes)
 		self.mesh_size = mesh_size
 		
-		self.grid = [[0 for col in range(sizes[1])] for row in range(sizes[0])]
-		self.grad_grid = [[0 for col in range(sizes[1])] for row in range(sizes[0])]
+		self.grid = np.zeros(sizes)
 		
-		for i in range(0,self.sizes[1],self.mesh_size[1]):
-			for j in range(0,self.sizes[0],self.mesh_size[0]):
-				self.grad_grid[i][j] = get_random_vector(self.dim)
-		
+		#Create a grad_grid with a size that allows each point from the grid to have four neighbour vectors
+		self.grad_grid = np.zeros(np.append(np.array(sizes) + np.array(mesh_size) + 1 - (np.array(sizes)%np.array(mesh_size)), self.dim))
 
-		for i in range(sizes[1]):
-			for j in range(sizes[0]):
-				self.grid[i][j] = self.point_perlin(j,i)
+		for y in range(0,self.grad_grid.shape[1],self.mesh_size[1]):
+			for x in range(0,self.grad_grid.shape[0],self.mesh_size[0]):
+				self.grad_grid[x,y] = get_random_vector(self.dim)
+		
+		
+		for y in range(sizes[1]):
+			for x in range(sizes[0]):
+				self.grid[x,y] = self.point_perlin(x,y)
 				
 
 	def save_as_image(self,file_name,file_path = ''):
@@ -43,7 +45,7 @@ class Perlin_Noise():
 		
 		for i in range(self.sizes[1]):
 			for j in range(self.sizes[0]):
-				pixels[i,j] = int(local_grid[i][j])
+				pixels[i,j] = int(local_grid[i,j])
 				
 		im.save(f'{file_path}/{file_name}.png')
 	
@@ -53,29 +55,29 @@ class Perlin_Noise():
 		
 		Args:
 			x: X coordinate
-			x: Y coordinate
+			y: Y coordinate
 			
 		Returns:
 			value: Value of the noise at this point
 		"""
 		
 		mesh_x0 = x  - (x % self.mesh_size[0])
-		mesh_x1 = min(mesh_x0 + self.mesh_size[0],self.sizes[1]-1)
+		mesh_x1 = min(mesh_x0 + self.mesh_size[0],self.grad_grid.shape[0]-1)
 		mesh_y0 = y  - (y % self.mesh_size[1])
-		mesh_y1 = min(mesh_y0 + self.mesh_size[1],self.sizes[0]-1)
+		mesh_y1 = min(mesh_y0 + self.mesh_size[1],self.grad_grid.shape[1]-1)
 
 		dx = (x - mesh_x0)/self.mesh_size[0]
 		dy = (y - mesh_y0)/self.mesh_size[1]
 
-		n0 = dot_product(self.grad_grid[mesh_y0][mesh_x0], [(-mesh_x0+x)/self.mesh_size[0], (-mesh_y0+y)/self.mesh_size[1]]);
-		n1 = dot_product(self.grad_grid[mesh_y0][mesh_x1], [(-mesh_x1+x)/self.mesh_size[0], (-mesh_y0+y)/self.mesh_size[1]]);
-		ix0 = interpolate(n0, n1, dx);
+		n0 = dot_product(self.grad_grid[mesh_x0,mesh_y0], [(-mesh_x0+x)/self.mesh_size[0], (-mesh_y0+y)/self.mesh_size[1]])
+		n1 = dot_product(self.grad_grid[mesh_x1,mesh_y0], [(-mesh_x1+x)/self.mesh_size[0], (-mesh_y0+y)/self.mesh_size[1]])
+		ix0 = interpolate(n0, n1, dx)
 
-		n0 = dot_product(self.grad_grid[mesh_y1][mesh_x0], [(-mesh_x0+x)/self.mesh_size[0], (-mesh_y1+y)/self.mesh_size[1]]);
-		n1 = dot_product(self.grad_grid[mesh_y1][mesh_x1], [(-mesh_x1+x)/self.mesh_size[0], (-mesh_y1+y)/self.mesh_size[1]]);
-		ix1 = interpolate(n0, n1, dx);
+		n0 = dot_product(self.grad_grid[mesh_x0,mesh_y1], [(-mesh_x0+x)/self.mesh_size[0], (-mesh_y1+y)/self.mesh_size[1]])
+		n1 = dot_product(self.grad_grid[mesh_x1,mesh_y1], [(-mesh_x1+x)/self.mesh_size[0], (-mesh_y1+y)/self.mesh_size[1]])
+		ix1 = interpolate(n0, n1, dx)
 		
-		value = interpolate(ix0, ix1, dy);
+		value = interpolate(ix0, ix1, dy)
 		return value
 	
 	def scale_grid(self,low = 0, high = 1):
@@ -91,14 +93,14 @@ class Perlin_Noise():
 			scaled_grid: 2D array of the scaled self.grid
 		"""
 		
-		scaled_grid = [[0 for col in range(self.sizes[1])] for row in range(self.sizes[0])]
+		scaled_grid = np.zeros(self.sizes)
 		
-		minimum = min([min(x) for x in self.grid])
-		maximum = max([max(x) for x in self.grid])
+		minimum = np.amin(self.grid)
+		maximum = np.amax(self.grid)
 		
-		for i in range(self.sizes[1]):
-			for j in range(self.sizes[0]):
-				scaled_grid[i][j] = (((self.grid[i][j]-minimum)*(high-low))/(maximum-minimum))+low
+		for y in range(self.sizes[1]):
+			for x in range(self.sizes[0]):
+				scaled_grid[x,y] = (((self.grid[x,y]-minimum)*(high-low))/(maximum-minimum))+low
 				
 		return scaled_grid
 		
@@ -108,11 +110,17 @@ class Perlin_Noise():
 		Should not be used with sizes bigger than 50*50, it can become really laggy (prefer show_map in this case)
 		
 		Args:
-			flatten_level: By how much should the data be flattened (default 1 = untouched).
+			flatten_level: By how much the data should be flattened (default 1 = untouched).
 			
 		Returns:
 			nothing
 		"""
+		
+		parameters = {
+			'stone_height': 1,
+			'water': True,
+			'water_level': 2,
+		}
 		
 		# define the coordinates system
 		x, y, z = np.indices((self.sizes[1],self.sizes[0], self.sizes[0]))
@@ -122,18 +130,37 @@ class Perlin_Noise():
 		cubelist = []
 		for i in range(self.sizes[1]):
 			for j in range(self.sizes[0]):
-				cube = (x == i) & (y == j) & (z <= (math.floor((self.sizes[0]-3)*local_grid[i][j])))
+				cube = (x == i) & (y == j) & (z <= (math.floor((self.sizes[0])*local_grid[j,i])))
 				cubelist.append(cube)
 					
 		# combine the objects into a single boolean array
 		voxelarray = np.bitwise_or.reduce(cubelist)
 		
 		# set the colors of each object (unused here)
-		colors = np.empty(voxelarray.shape, dtype=object)
+		colors = np.zeros(voxelarray.shape + (3,))
+		
+		#set all colors to green
+		colors[:,:,:, 0] = .06
+		colors[:,:,:, 1] = .35
+		colors[:,:,:, 2] = .10
+		
+		
+		if parameters['water']:
+			#set all blocks with z<=water_level to blue		
+			for x in range(colors.shape[0]):
+				for y in range(colors.shape[1]):
+					for z in range(parameters['water_level']):
+						voxelarray[x,y,z] = True
+						voxelarray[x,y,z] = True
+						voxelarray[x,y,z] = True
+						if voxelarray[x,y,min(z+1,voxelarray.shape[2]-1)] == False: #if there is no block over
+							colors[x,y,:z+1, 0] = 0
+							colors[x,y,:z+1, 1] = 0
+							colors[x,y,:z+1, 2] = .5
 		
 		# plot everything
 		ax = plt.figure().add_subplot(projection='3d')
-		ax.voxels(voxelarray, facecolors='green', edgecolor='k')
+		ax.voxels(voxelarray, facecolors = colors,edgecolor='k')
 
 		plt.show()
 	
@@ -180,7 +207,6 @@ def get_random_vector(n):
 		angle_1 = random.random()*2*math.pi
 		s = math.sqrt(2)
 		return [1*math.cos(angle_1),1*math.sin(angle_1)]
-		#return random.choice([[s,s],[s,-s],[-s,-s],[-s,s]])
 		
 def smoothstep(w):
 	if (w <= 0.0):
@@ -196,6 +222,7 @@ def interpolate(a, b, weight):
 
 
 if __name__ == '__main__':
-	p = Perlin_Noise([301,301],[50,50])
+	p = Perlin_Noise([30,30],[20,20])
 	#p.save_as_image("image","mon_perlin1")
-	p.show_map(4)
+	p.show_voxel_map(3)
+	#p.show_map(3)
